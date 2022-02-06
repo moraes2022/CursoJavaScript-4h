1867b030c393b7ebc43ee4c6fbeccaa1d11b773b
Mr. Moraes <mr.moraess@outlook.com>
2022-02-06 12:42:53 -0300

    Treinamento 5: Arrays

--- aula.js
+++ aula.js
@@ -1,18 +1,44 @@
 console.log('%c Hello World ','color:hsl(240, 100%, 90%);background-color:hsl(240, 100%, 50%);');
 console.log("-".repeat(589))
 
-console.log("Treinamento 3: Tipos primitivos - Referencia")
+console.log("Treinamento 5 - Arrays")
 
 console.log("-".repeat(589))
 
+let familia = [true, 45,'jhonatan', 17];
+console.log(familia);
+console.log(familia.length);
+console.log(familia[0]);
 
+console.log('Exercício proposto');
+
+let nomeDoColega = ['Eriberg', 37,'casado', '2 filhos', 'cidade residente: Iranduba'];
+console.log(nomeDoColega);
+
+
+
+
+
+
+
+/*console.log("variavéis exemplo abaixo")
 let nome = "Rafael"; // string literal
 let idade = 25; // number literal
 let estaAprovado = true; //boolean
 let sobreNome = undefined; // undefined
-let corSelecionado = null; // Rdefinir um valor
+console.log(nome);
+console.log(idade);
+console.log(estaAprovado);
+console.log(sobreNome);
 
-console.log(nome, typeof nome);
-console.log(idade, typeof idade);
-console.log(estaAprovado, typeof estaAprovado);
-console.log(corSelecionado, typeof corSelecionado)
+console.log("objeto, exemplo abaixo")
+let pessoa = {
+    nome: "Rafael",
+    idade: 25,
+    estaAprovado: true,
+    sobreNome: undefined
+}
+
+console.log(pessoa)*/
+
+
