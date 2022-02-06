016a078b3b1524763eb8651ec683b38fa3181f0a
Mr. Moraes <mr.moraess@outlook.com>
2022-02-06 13:31:46 -0300

    Treinamento 6: Functions

--- aula.js
+++ aula.js
@@ -1,44 +1,36 @@
 console.log('%c Hello World ','color:hsl(240, 100%, 90%);background-color:hsl(240, 100%, 50%);');
 console.log("-".repeat(589))
 
-console.log("Treinamento 5 - Arrays")
+console.log("Treinamento 6 - Functions")
 
 console.log("-".repeat(589))
-
-let familia = [true, 45,'jhonatan', 17];
-console.log(familia);
-console.log(familia.length);
-console.log(familia[0]);
-
-console.log('Exercício proposto');
-
-let nomeDoColega = ['Eriberg', 37,'casado', '2 filhos', 'cidade residente: Iranduba'];
-console.log(nomeDoColega);
-
-
-
-
-
-
-
-/*console.log("variavéis exemplo abaixo")
-let nome = "Rafael"; // string literal
-let idade = 25; // number literal
-let estaAprovado = true; //boolean
-let sobreNome = undefined; // undefined
-console.log(nome);
-console.log(idade);
-console.log(estaAprovado);
-console.log(sobreNome);
-
-console.log("objeto, exemplo abaixo")
-let pessoa = {
-    nome: "Rafael",
-    idade: 25,
-    estaAprovado: true,
-    sobreNome: undefined
+console.log("//Verbo + substantivo")
+let corSite = "Azul";
+function resetaCor(){
+    corSite = " "
 }
+console.log("let corSite = " + corSite)
+console.log("function resetaCor(){");
+console.log("corSite = '' ");
+console.log("}");
+console.log(corSite);
+resetaCor()
+console.log(corSite + "sem cor")
 
-console.log(pessoa)*/
+    /*let corSite = "azul";
+    function resetaCor(cor, tonalidade){
+        corSite = cor + tonalidade;
+}
+console.log(corSite);
+resetaCor("verde", " claro");
+console.log(corSite);
+*/
 
+console.log("exercicio proposto")
+function somar(a, b){
+return a + b
+}
+console.log(somar);
+console.log("a=1.4, b=10*3")
+console.log("a+b = " + somar(1.4, 10*3))
 
